47d767d92f466b4da741e62d6afe6868274e6782
Rajasekhar2319 <95023313+Rajasekhar2319@users.noreply.github.com>
2022-06-22 07:09:31 +0530

    Delete Airflow_Dag_Spark_Submit.py

--- Airflow_Dag_Spark_Submit.py
+++
@@ -1,9 +0,0 @@
-import airflow
-
-from airflow.models import DAG
-from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
-
-dag = DAG(
-	dag_id = "Airflow_dag_Spark_Submit",schedule_interval = '* * * * *'
-
-	)
